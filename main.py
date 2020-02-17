af2dd54651205faf4e305678b08265f4d6e4cc49
kulvirvirk <kvirkdev@gmail.com>
2020-02-17 05:37:57 +0000

    added an example of add() method

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # 1. create and print set that contain some fruits
+# 2. add more elements to set with add() method
 
-
-
+# 1. create and print set that contain some fruits
 my_fruit_set = {'banana', 'mango', 'orange', 'kiwi', 'apple'}
 print(my_fruit_set)
+print('--------------******--------------\n')
+
+# 2. add more elements to set with add() method
+my_fruit_set.add('watermelon')
+print(my_fruit_set)
+print('--------------******--------------\n')
+
