7bc1a2cf2301581da736906428be64040db2ef27
kulvirvirk <kvirkdev@gmail.com>
2020-02-17 05:41:40 +0000

    added code to test - if duplicate element can be added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # 1. create and print set that contain some fruits
 # 2. add more elements to set with add() method
+# 3. try to add a duplicate element in set, does it gets added?  
 
 # 1. create and print set that contain some fruits
 my_fruit_set = {'banana', 'mango', 'orange', 'kiwi', 'apple'}
@@ -11,3 +12,9 @@ my_fruit_set.add('watermelon')
 print(my_fruit_set)
 print('--------------******--------------\n')
 
+# 3. try to add a duplicate element in set, does it gets added?  
+#   - no, the Set has unique elements - has no duplicate elements
+my_fruit_set.add('blueberry')
+my_fruit_set.add('blueberry')
+print(my_fruit_set)
+print('--------------******--------------\n')
